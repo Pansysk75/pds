ee272f728cb52fc2b5d3531a2389d95bbb5853e6
Panos <pansysk75@gmail.com>
2022-11-10 23:16:37 +0200

    implement CSR graph, and some progress on coloring

--- src/DirectedGraph.hpp
+++ src/DirectedGraph.hpp
@@ -3,11 +3,20 @@
 #include <iostream>
 #include <fstream>
 #include <cstring>
+#include <stdexcept>
+#include <numeric>
 
-struct DirectedEdge{
+
+
+
+
+
+
+struct DirectedEdge {
     unsigned int from;
     unsigned int to;
 };
+
 
 struct DirectedGraph{
     // DirectedGraph "contains" all nodes 1,2,3, ... , size
@@ -16,14 +25,20 @@ struct DirectedGraph{
     unsigned int size;
     std::vector<DirectedEdge> edges;
 
+    DirectedGraph() 
+        :size(0) 
+    {
 
+    }
     DirectedGraph(std::string filename){
 
         // Get a directed graph by parsing 
         // a .mtx (sparse matrix) file
         std::ifstream file(filename);
         std::string line;
-
+        if (!file.good()) {
+            throw std::invalid_argument("Unable to read file: " + filename);
+        }
 
         unsigned int rows, cols, n_values;
         // discard comments and first line of integers, which
@@ -36,14 +51,16 @@ struct DirectedGraph{
         }
         
         file >> rows >> cols >> n_values;
+
         this->size = rows;
 
         unsigned int weight, from, to;
         //read file while ignoring lines that are not 
         //in the form "integer integer integer"
         while(!file.eof()){ 
-            if (file >> weight >> from >> to){
-                this->edges.push_back(DirectedEdge{from, to});
+            if (file >> from >> to >> weight){
+                //Subtract 1 so that node ids start from 0
+                this->edges.push_back(DirectedEdge{to-1, from-1});
                 // std::cout << "S" ;
             }
             else{
@@ -54,4 +71,46 @@ struct DirectedGraph{
         }
     }
 };
+
+
+struct GraphCSR {
+    //stores directed graph edges in "Compressed Sparse Row" format
+    std::vector<unsigned int> vec_from_idx;
+    std::vector<unsigned int> vec_to;
+    unsigned int max_node_id;
+
+    GraphCSR(DirectedGraph& graph_coo) {
+        auto& edges = graph_coo.edges;
+        /*
+        //doesn't assume graph_coo edges are sorted in any way
+
+        //counts[i] will contain the amount of edges that satistfy from==i
+        std::vector<unsigned int> counts(graph_coo.max_node_id);
+        for (auto& edge : edges) {
+            counts[edge.from]++;
+        }
+
+        from_idx.resize(counts.size() + 1);
+        from_idx = std::exclusive_scan(); //todo
+        */
+
+        //LETS ASSUME PRIMARY SORT -> TO, SECONDARY SORT -> FROM
+        int n_nodes = graph_coo.size;
+        vec_to.reserve(edges.size());
+        std::vector<unsigned int> counts(n_nodes, 0);
+        for (auto& edge : edges) {
+            counts[edge.from]++;
+            vec_to.push_back(edge.to);
+        }
+        vec_from_idx.resize(n_nodes + 1, 0);
+        vec_from_idx[0] = 0;
+        //accumulate count
+        for (int i = 0; i < n_nodes; i++) {
+            vec_from_idx[i + 1] = vec_from_idx[i] + counts[i];
+        }
+
+
+    }
+};
+
 
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,48 @@
 #include "DirectedGraph.hpp"
+#include <numeric>
+#include <algorithm>
 
 int main(){
-    DirectedGraph graph("celegansneural.mtx");
-    //todo: Coloring Algorithm
+    // DirectedGraph graph("../celegansneural.mtx");
+    DirectedGraph graph;
+    graph.size = 6;
+    graph.edges = { 
+        {0,1}, {1,2},
+        {1,4}, {1,5},
+        {2,3}, {3,5},
+        {4,0}, {5,2},
+         };
+    
+    GraphCSR csr_graph(graph);
+
+    // Fill colors with 0,1,2...n so that,
+    // for nodes 0,1,2...n,  colors[i] = i;
+    std::vector<unsigned int> colors(graph.size);
+    std::iota(colors.begin(), colors.end(), 0);
+
+    // Each node will be assigned an integer according to 
+    // which SCC it belongs. Nodes belonging to the same SCC
+    // will be assigned the same integer.
+    // -1 means that the node hasn't been assigned to a SCC yet.
+    std::vector<int> groups(graph.size, -1);
+
+    bool changed_color = true;
+    while(changed_color){
+        changed_color = false;
+        for(auto& elem : graph.edges){
+            unsigned int from_color = colors[elem.from];
+            unsigned int to_color = colors[elem.to];
+            if (to_color > from_color){
+               colors[elem.to] = from_color;
+               changed_color = true;
+            }
+        }
+    }
+    unsigned int n_nodes_assigned = 0;
+    while (n_nodes_assigned < graph.size) {
+        //TODO
+    }
+
+
     
 }
